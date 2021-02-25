fafda129f77593522e8d1371dc740b2439baa6c2
chisoftltd <chisoftltd@gmail.com>
2021-02-25 17:45:01 +0000

    DataFrames

--- PandasSeries.py
+++ PandasSeries.py
@@ -20,3 +20,27 @@ print()
 
 # When you have created labels, you can access an item by referring to the label
 print(myvar["y"])
+print()
+
+# Key/Value Objects as Series
+calories = {"day1": 420, "day2": 380, "day3": 390}
+myvar = pd.Series(calories)
+
+print(myvar)
+print()
+
+# Create a Series using only data from "day1" and "day2"
+myvar = pd.Series(calories, index = ["day1", "day2"])
+
+print(myvar)
+print()
+
+# DataFrames
+data = {
+  "calories": [420, 380, 390],
+  "duration": [50, 40, 45]
+}
+
+myvar = pd.DataFrame(data)
+
+print(myvar)
